7a98f2112c8d88d043305bec059f61c0a58922ea
wangjie <17054214@qq.com>
2020-05-28 18:07:32 +0800

    Initial Commit

---
+++ SStickDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SStickDemo
+//
+//  Created by 王杰 on 2020/5/28.
+//  Copyright © 2020 王杰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
